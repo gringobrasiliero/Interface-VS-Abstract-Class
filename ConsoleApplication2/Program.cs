840d836ddcfb7612016eafb71561d0f2ffdad79c
Nolan Byrnes <nolanehs@gmail.com>
2018-10-22 07:59:01 -0400

    Abstract class

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -4,12 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication2
+namespace AbstractClassDemo
 {
+    abstract class iPhone
+    {
+        //Non-Abstract Method   
+        public void Call()
+        {
+            Console.WriteLine("Call Method: This method provides Calling features");
+        }
+
+        //Abstract Method   
+        public abstract void Model();
+    }
+
+    class iPhone5s : iPhone
+    {
+        //Abstract Method implementation
+        public override void Model()
+        {
+            Console.WriteLine("This is a iPhone 5s.");
+        }
+
+        public void LaunchDate()
+        {
+            Console.WriteLine("Launch date: 9/20/2013");
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            iPhone5s iphone5s = new iPhone5s();
+            iphone5s.Call();
+            iphone5s.Model();
+            iphone5s.LaunchDate();
+            Console.ReadKey();
         }
     }
 }
